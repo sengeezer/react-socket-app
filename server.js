3f91c5a439fbb5429bbff380c864f6f3ff69b1af
Fabio Albertin <albertin@gmail.com>
2019-11-21 22:54:07 +0000

    Added initial server code

---
+++ server.js
@@ -0,0 +1,20 @@
+import express from 'express';
+import http from 'http';
+import socketIO from 'socket.io';
+
+const port = 4001;
+
+const app = express();
+const server = http.createServer(app);
+
+const io = socketIO(server);
+
+io.on('connection', (socket) => {
+  console.log('User connected');
+
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+});
+
+server.listen(port, () => console.log(`Listening on port ${port}...`));
